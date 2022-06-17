866f5d3916cf4a7cf7d11ea25f2e8a9bd182b971
Dzianis Dashkevich <Dzianis.Dashkevich@vizio.com>
2022-06-17 13:26:04 -0700

    implement addTwo

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,7 +1,15 @@
-import { sum } from "../src/utils.js";
+import { addTwo, sum } from "../src/utils.js";
 
-describe('sum', () => {
-    it('should add 2 digits', () => {
-        expect(sum(2, 2)).toBe(4);
+describe('utils', () => {
+    describe('sum', () => {
+        it('should add 2 digits', () => {
+            expect(sum(2, 2)).toBe(4);
+        });
+    });
+
+    describe('addTwo', () => {
+        it('should add 2 to any digit', () => {
+            expect(addTwo(4)).toBe(6);
+        });
     });
 });
